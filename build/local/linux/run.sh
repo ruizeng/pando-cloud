accd99ac5f62f178f3850121edfa73857dad86e0
razr <razr.china@gmail.com>
2015-11-24 17:09:42 +0800

    local start script : run in background

--- build/local/linux/run.sh
+++ build/local/linux/run.sh
@@ -4,9 +4,9 @@
 echo 'CREATE DATABASE PandoCloud' | mysql -uroot 
 
 # start services
-$GOPATH/bin/httpaccess -etcd http://localhost:2379 -rpchost localhost:20035 -httphost :8000 -loglevel debug
-$GOPATH/bin/registry -etcd http://localhost:2379 -rpchost localhost:20034 -aeskey ABCDEFGHIJKLMNOPABCDEFGHIJKLMNOP -dbhost localhost -dbname PandoCloud -dbport 3306 -dbuser root -loglevel debug
-$GOPATH/bin/apiprovider -etcd http://localhost:2379 -httphost localhost:8888
-$GOPATH/bin/devicemanager -etcd http://localhost:2379 -rpchost localhost:20033
-$GOPATH/bin/controller -etcd http://localhost:2379 -rpchost localhost:20032
+$GOPATH/bin/httpaccess -etcd http://localhost:2379 -rpchost localhost:20035 -httphost :8000 -loglevel debug &
+$GOPATH/bin/registry -etcd http://localhost:2379 -rpchost localhost:20034 -aeskey ABCDEFGHIJKLMNOPABCDEFGHIJKLMNOP -dbhost localhost -dbname PandoCloud -dbport 3306 -dbuser root -loglevel debug &
+$GOPATH/bin/apiprovider -etcd http://localhost:2379 -httphost localhost:8888 &
+$GOPATH/bin/devicemanager -etcd http://localhost:2379 -rpchost localhost:20033 &
+$GOPATH/bin/controller -etcd http://localhost:2379 -rpchost localhost:20032 &
 
