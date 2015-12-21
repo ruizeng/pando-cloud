03e02a586cf1a42a62c8afa53cd288810d63fb07
razr <razr.china@gmail.com>
2015-12-21 16:28:16 +0800

    change quick start run.sh, apiprovider http host bind to all interface.

--- build/local/linux/run.sh
+++ build/local/linux/run.sh
@@ -5,7 +5,7 @@ killall -9 httpaccess registry apiprovider devicemanager controller mqttaccess
 # start services
 $GOPATH/bin/httpaccess -etcd http://localhost:2379 -httphost internal:443 -loglevel debug -usehttps -keyfile $GOPATH/src/github.com/PandoCloud/pando-cloud/pkg/server/testdata/key.pem -cafile $GOPATH/src/github.com/PandoCloud/pando-cloud/pkg/server/testdata/cert.pem &
 $GOPATH/bin/registry -etcd http://localhost:2379 -rpchost localhost:20034 -aeskey ABCDEFGHIJKLMNOPABCDEFGHIJKLMNOP -dbhost localhost -dbname PandoCloud -dbport 3306 -dbuser root -loglevel debug &
-$GOPATH/bin/apiprovider -etcd http://localhost:2379 -loglevel debug  -httphost internal:8888 &
+$GOPATH/bin/apiprovider -etcd http://localhost:2379 -loglevel debug  -httphost :8888 &
 $GOPATH/bin/devicemanager -etcd http://localhost:2379 -loglevel debug  -rpchost localhost:20033 &
 $GOPATH/bin/controller -etcd http://localhost:2379 -loglevel debug  -rpchost localhost:20032 &
 $GOPATH/bin/mqttaccess -etcd http://localhost:2379 -loglevel debug  -rpchost localhost:20030 -tcphost internal:1883 -usetls -keyfile $GOPATH/src/github.com/PandoCloud/pando-cloud/pkg/server/testdata/key.pem -cafile $GOPATH/src/github.com/PandoCloud/pando-cloud/pkg/server/testdata/cert.pem &
